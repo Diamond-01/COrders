6b0851d0d6e5879014355611570e14b6af3606cb
Mirio <mariorosero3801@gmail.com>
2026-02-03 19:50:14 -0500

    Campos
    Definiendo Campos a Typescript

---
+++ order-builder/src/domain/fields/Field.ts
@@ -0,0 +1,7 @@
+import { TextField } from "./TextField";
+import { NumberField } from "./NumberField";
+import { DateField } from "./DateField";
+import { SelectField } from "./SelectField";
+
+export type Field = TextField | NumberField | DateField | SelectField;
+
