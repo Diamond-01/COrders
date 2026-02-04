b9c6b8889ea7af97030a3de2e372bf0185ab008b
Mirio <mariorosero3801@gmail.com>
2026-02-03 19:13:39 -0500

    Campo de Números + crear
    interface Campo Número y metodo crear

---
+++ order-builder/src/domain/fields/NumberField.ts
@@ -0,0 +1,18 @@
+import { BaseField } from "./BaseField";
+import { FieldTypes } from "./FieldTypes";
+
+export interface NumberField extends BaseField{
+    type: FieldTypes.NUMBER;
+    min: number | null;
+    max: number | null;
+}
+
+export const crearNumberField = (id:string, overrides?:Partial<NumberField>): NumberField => ({
+    id,
+    type: FieldTypes.NUMBER,
+    label: "Número",
+    requeired: false,
+    min: null,
+    max: null,
+    ...overrides,
+});     
